28f68b38b2d7c125b4d98e1146f2713bb0e1a925
HeonSIk Lee <90516010+LeeHeonSik@users.noreply.github.com>
2021-09-17 01:42:02 +0000

    Sync LeetCode submission - Maximum Number of Words You Can Type (python3)

--- problems/maximum_number_of_words_you_can_type/solution.py
+++ problems/maximum_number_of_words_you_can_type/solution.py
@@ -1,11 +1,8 @@
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
-        L = list(map(str, text.split(' ')))
-        ans = len(L)
-        for i in range(len(L)):
-            for j in brokenLetters:
-                if j in L[i]:
-                    ans -=1
-                    break
+        ans = 0
+        L = list(text.split(' '))
+        for i in L:
+            if all(j not in i for j in brokenLetters):
+                ans+=1
         return ans
-        
