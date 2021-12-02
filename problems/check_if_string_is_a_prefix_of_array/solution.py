76eec6a1d3235bab04094d044397d4c6b865142e
HeonSIk Lee <90516010+LeeHeonSik@users.noreply.github.com>
2021-12-02 03:15:56 +0000

    Sync LeetCode submission - Check If String Is a Prefix of Array (python3)

---
+++ problems/check_if_string_is_a_prefix_of_array/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPrefixString(self, s: str, words: List[str]) -> bool:
+        ans = ""
+        for i in words:
+            ans += i
+            if ans == s:
+                return True
+            if ans not in s:
+                return False
+        return False
