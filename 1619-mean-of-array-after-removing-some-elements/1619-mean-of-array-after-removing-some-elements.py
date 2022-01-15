e77719a8853a4931b987df5a6d6585ebd6e3aaa9
HeonSIk Lee <90516010+LeeHeonSik@users.noreply.github.com>
2022-01-15 15:00:22 +0900

    Time: 56 ms (84.10%), Space: 14.5 MB (8.89%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.py
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.py
@@ -1,10 +1,9 @@
 class Solution:
     def trimMean(self, arr: List[int]) -> float:
         k = len(arr) // 20
-        for i in range(k):
-            arr.remove(min(arr))
-            arr.remove(max(arr))
-            
-        return round((sum(arr) / len(arr)),6)
+        arr.sort()
+        l = arr[k:-k]
+        return sum(l)/len(l)
+        # return round((sum(l) / l),6)
         
         
