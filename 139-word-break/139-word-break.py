7426f39d6fbb4ad90b3eb3473abff4584dc308d3
HeonSIk Lee <90516010+LeeHeonSik@users.noreply.github.com>
2022-02-02 12:48:37 +0900

    Time: 50 ms (46.90%), Space: 14.1 MB (91.27%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,18 +1,19 @@
 class Solution:
     def dp(self, s, wordDict, memo):
         if s in memo:
-            return memo[s];
+            return memo[s]
         if s == "":
-            return True;
+            return True
 
         for i in wordDict:
             if s.find(i) == 0:
-                memo[s] = self.dp(s[len(i)::], wordDict, memo);
+                memo[s] = self.dp(s[len(i)::], wordDict, memo)
                 if memo[s] == True:
-                    return True;
+                    return True
 
-        memo[s] = False;
-        return memo[s];
+        memo[s] = False
+        return memo[s]
 
+    
     def wordBreak(self, s, wordDict):
-        return self.dp(s, wordDict, {});
+        return self.dp(s, wordDict, {})
