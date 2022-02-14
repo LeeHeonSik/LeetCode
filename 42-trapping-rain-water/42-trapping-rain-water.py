808aee59c456b79fca672984d63e5846e7e710e8
HeonSIk Lee <90516010+LeeHeonSik@users.noreply.github.com>
2022-02-14 23:30:26 +0900

    Time: 4614 ms (5.01%), Space: 15.8 MB (33.91%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -2,8 +2,6 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         ans = 0
         le = 0
-        fstmax = max(height)
-        idx = height.index(fstmax)
         
         for i in range(len(height)):
             ri = 0
@@ -12,10 +10,7 @@ class Solution:
                 le = height[i]
                 
             else:
-                if i < idx:
-                    ans += min(le, fstmax) - height[i]
-                else:
-                    ri = max(height[i::])
-                    ans += min(le, ri) - height[i]
+                ri = max(height[i::])
+                ans += min(le, ri) - height[i]
                 
         return ans
