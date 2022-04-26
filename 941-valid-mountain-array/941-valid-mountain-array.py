e452be3a10e123a9efb9b1dc20af1d740fbb02fe
HeonSIk Lee <90516010+LeeHeonSik@users.noreply.github.com>
2022-04-26 10:26:26 +0900

    Time: 200 ms (97.09%), Space: 15.5 MB (24.61%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.py
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -1,26 +1,12 @@
-def inc(idx, arr):
-    for i in range(1, idx):
-           if arr[i-1] >= arr[i]:
-                return False
-    return True
-
-def dec(idx, arr):    
-    for j in range(idx, len(arr)-1):
-        if arr[j] <= arr[j+1]:
-            return False
-    return True
-
-class Solution:  
+class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        if len(arr) < 3 or arr[0] > arr[1]:
+        n = arr.index(max(arr))
+        if n == 0 or n == len(arr)-1:
             return False
-        idx = 0
-        for i in range(1, len(arr)):
+        for i in range(1,n):
             if arr[i-1] >= arr[i]:
-                idx = i-1
-                break
-                
-        left = inc(idx, arr)
-        right = dec(idx, arr)
-        
-        return left == right == True
+                return False
+        for j in range(n+1,len(arr)):
+            if arr[j-1] <= arr[j]:
+                return False
+        return True
