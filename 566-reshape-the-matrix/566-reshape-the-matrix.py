af1690ef570f72274de1c829e81370680b4a3696
HeonSIk Lee <90516010+LeeHeonSik@users.noreply.github.com>
2022-07-04 09:48:37 +0900

    Time: 294 ms (6.33%), Space: 32.7 MB (5.04%) - LeetHub

---
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -0,0 +1,9 @@
+import numpy as np
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        if m * n != r * c:
+            return mat
+        
+        else:
+            return np.reshape(mat,(r,c))
